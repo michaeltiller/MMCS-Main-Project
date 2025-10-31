b1323878d451a0702e228013797e8364f819945b
Naoise <s2848034@ed.ac.uk>
2025-10-31 09:57:12 +0000

    document helper funcs

--- helper_functions.py
+++ helper_functions.py
@@ -1,6 +1,8 @@
 import os, datetime
 def path( *args, **kwargs):
+     """returns an OS independent path out of its arguments"""
      return os.path.join(*args, **kwargs)
 
 def timestamp():
+     """returns a string timestamp of the day and hour right now"""
      return datetime.datetime.now().strftime('%d %b %Y %I-%M%p')
