2c04f95d93c52462de4f9a08754c025db84d5dff
Naoise <s2848034@ed.ac.uk>
2025-11-18 17:26:53 +0000

    fix bug in create_colours_for_locs_and_assignments

--- helper_functions.py
+++ helper_functions.py
@@ -19,31 +19,36 @@ def get_MIP_gap(prob):
           mip_gap = float('inf')
      return mip_gap
 
-def create_colours_for_locs_and_assignments(locs,assign_labs, cmap_name = "cool"):
+def create_colours_for_locs_and_assignments(locs,assignments, cmap_name = "cool"):
     """
     This is for colour assignment when there are certain number of locations ``locs`` 
-    and many other points have been assigned to a unique location, as recorded in ``assign_labs``.
+    and many other points have been assigned to a unique location, as recorded in ``assign``.
 
-    E.g two locations and five points a possible input is assign_labs=[0,1,1,0,0] and possible output is 
+    E.g two locations and five points a possible input is assign=[0,1,1,0,0] and possible output is 
     ["blue", "green"], ["blue","green","green","blue","blue"]
 
     Locations with no points assigned are coloured in gray
     """
-    locs_with_assignments = np.unique(assign_labs)
+    locs_ass, assignments = np.unique(assignments, return_inverse=True)
+    
+    locs_to_ind = dict( (l, i) for i, l in enumerate(locs))
+    ind_locs_ass = np.array([ locs_to_ind[l] for l in locs ])
+    print(f"{ind_locs_ass=}")
     # will need to do something here when number of locs is large
     # the colours will differ by increasingly smpall degrees
     # an alternative is to limit the size of the cmap to say 20 and shuffle the indexes before being passed
     # so location 5 wont be mapped to index 4 anymore and wont get a colour similar to location 6
-    cmap = plt.colormaps[cmap_name].resampled( len(locs_with_assignments) )
-    cols_for_locs_with_demand  = cmap(np.arange(len(locs_with_assignments)))
+    cmap = plt.colormaps[cmap_name].resampled( len(locs_ass) )
+    cols_locs_ass  = cmap(np.arange(len(locs_ass)))
 
 
     cols_for_locs = np.zeros((len(locs),4))
+    print(f"{cols_for_locs.shape=}")
     # locations with no demand/desire are shown in grey
     cols_for_locs[:,:] = .5
 
-    cols_for_locs[locs_with_assignments,:] = cols_for_locs_with_demand
+    cols_for_locs[ind_locs_ass,:] = cols_locs_ass
 
-    cols_for_points = cols_for_locs[locs_with_assignments]
+    cols_for_points = cols_for_locs[assignments]
 
     return cols_for_locs, cols_for_points
