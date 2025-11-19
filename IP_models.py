c5a977d1ae8e6ba576bfd96202b4de6952fabbdd
Naoise <s2848034@ed.ac.uk>
2025-11-19 00:46:02 +0000

    add demo code for new IP model in run_the_code_file

--- IP_models.py
+++ IP_models.py
@@ -1,6 +1,7 @@
 import xpress as xp
 import pandas as pd
 import numpy as np
+from shapely import Point
 from time import perf_counter
 from helper_functions import *
 import platform
@@ -9,7 +10,7 @@ if platform.system() == "Windows":
     xp.init('C:/xpressmp//bin/xpauth.xpr')
 
 
-def create_and_solve_model(desire, dist_mat, bike_max, cost_bike, cost_station, budget, dist_max=5_000 ):
+def create_and_solve_first_model(desire, dist_mat, bike_max, cost_bike, cost_station, budget, dist_max=5_000 ):
 
     # stop the big stream of text
     xp.setOutputEnabled(False)
@@ -93,27 +94,146 @@ def create_and_solve_model(desire, dist_mat, bike_max, cost_bike, cost_station, 
     return solution, MIP_gap, near
 
 
-if __name__ == "__main__":
-    #read in data here as np.arrays
-    folder = path("good_example_data", "05 Nov 2025 03-50PM")
-    # demand = np.load(path(folder, "demand.npy"))
-    all_dists = np.load(path(folder, "all_dists.npy"))
 
-    # that demand data is wonky so ill just make up data
-    from scipy.stats import poisson
-    demand = poisson.rvs(size = all_dists.shape[0], mu = 5, random_state = 2025)
+def create_and_solve_extended_model(desire, dist_mat, near_centre, cost_bike, cost_station, budget,
+                                     dist_max=5, dist_min_in_centre = .250, dist_min_outside_centre =.750,
+                                      bikes_max = 50, bikes_total = 800 ):
 
-    r = 15
-    all_dists = all_dists[:r,:r]
-    demand = demand[:r]
+    print("setting up in IP")
+    s= perf_counter()
+    # stop the big stream of text
+    xp.setOutputEnabled(False)
+    prob = xp.problem("First_bike_extension") 
 
+    num_locs = desire.shape[0]
+    I = range(num_locs)
 
-    sol, mip, near = create_and_solve_model(
-        desire=demand, dist_mat=all_dists, bike_max=50,
-        cost_bike=580, cost_station=20_000, budget=2_000_000,
-        dist_max=5_000
+    ########### Decision variables #############
+
+    # we have the binary decision variable to build a station at location i
+    build = np.array(
+        [prob.addVariable(name = f"build_{i}", vartype = xp.binary) for i in I ]
+        ,dtype = xp.npvar
     )
-    print(sol)
-    near = np.array(near, dtype=int)
-    print(near)
+    #we have the integer decision variable of how many bikes to place at location i
+    bikes = np.array(
+        [prob.addVariable(name = f"bikes_{i}", vartype= xp.integer) for i in I]
+        , dtype=xp.npvar
+    )
+
+    ########### objective function #############
+    prob.setObjective(
+        xp.Sum( desire[i]*bikes[i] for i in I )
+        , sense = xp.maximize
+    )
+
+    ########### constraints #############
+
+    # we can place at most bikes_max bikes in each location
+    # and if we put bikes somewhere then we must build a station there
+    prob.addConstraint(
+        bikes[i] <= bikes_max*build[i] for i in I
+    )
+
+    # we only have bikes_total bikes
+    prob.addConstraint(
+        xp.Sum(bikes[i] for i in I) <= bikes_total
+    )
+
+    # If we build a station somewhere then we must put at least one bike there
+    prob.addConstraint(
+        build[i] <= bikes[i] for i in I
+    )
+    
+    # we will not put more bikes in a location than there is desire for bikes
+    # the plus one is to avoid issues with combining the above constraint and the connectedness constraint
+    # prob.addConstraint(
+    #     bikes[i] <= desire[i] + 1 for i in I
+    # )
+
+    # Every station must have at least one near it
+    near = dist_mat <= dist_max
+    prob.addConstraint(
+        build[i] <= xp.Sum( near[i,j]*build[j] for j in I if j != i)
+        for i in I
+    )
+
+    # stations cannot be too close together
+    # there is a different cut-off for too close, inside and outside of the city centre
+    too_close = np.zeros((num_locs, num_locs), dtype=np.bool)
+    for i in I:
+        if near_centre[i]:
+            cutoff = dist_min_in_centre
+        else:
+            cutoff = dist_min_outside_centre
+        new = dist_mat[i] <= cutoff
+        too_close[i] = new
+
+    prob.addConstraint(
+        build[i] + xp.Sum( too_close[i,j]*build[j] for j in I if j !=i ) <= 1
+        for i in I
+    )
+
+    # stay within budget
+    prob.addConstraint(
+        xp.Sum( cost_bike*bikes[i] + cost_station*build[i] for i in I) <= budget
+    )
+
+    ########## Solving ###########
+    # Write problem statement to file, for debugging
+    # prob.write("problem","ips")
+    e = perf_counter()
+    print(f"set-up in IP took {e-s:.0f} seconds")
+
+    print("Solving")
+    solve_start = perf_counter()
+    prob.solve()
+    solve_end = perf_counter()
+    print(f"Solved in {solve_end-solve_start:.0f} seconds with {desire.shape[0]:,} variables")
+
+    #mip gap 
+    MIP_gap= get_MIP_gap(prob)
+    print(f"{MIP_gap=:.2%}")
+
+    # look at the solution
+    solution  = pd.DataFrame({
+        "build": np.array([ int(i) for i in prob.getSolution(build) ]),
+        "bikes": np.array([int(i) for i in prob.getSolution(bikes) ]),
+        "desire": desire
+    })
+
+    # return the pertient info that was not inputted
+    return solution, MIP_gap, near
+
+
+
+if __name__ == "__main__":
+
+    # ## look at the first model
+    # #read in data here as np.arrays
+    # folder = path("good_example_data", "05 Nov 2025 03-50PM")
+    # # demand = np.load(path(folder, "demand.npy"))
+    # all_dists = np.load(path(folder, "all_dists.npy"))
+
+    # # that demand data is wonky so ill just make up data
+    # from scipy.stats import poisson
+    # demand = poisson.rvs(size = all_dists.shape[0], mu = 5, random_state = 2025)
+
+    # r = 15
+    # all_dists = all_dists[:r,:r]
+    # demand = demand[:r]
+
+
+    # sol, mip, near = create_and_solve_first_model(
+    #     desire=demand, dist_mat=all_dists, bike_max=50,
+    #     cost_bike=580, cost_station=20_000, budget=2_000_000,
+    #     dist_max=5_000
+    # )
+    # print(sol)
+    # near = np.array(near, dtype=int)
+    # print(near)
+
+    ### look at the model with min and max distances and bikes
+
+    pass
 
--- run_the_code.py
+++ run_the_code.py
@@ -12,78 +12,136 @@ BUDGET = 400_000
 DIST_MAX = 2_000
 
 
-###### read in POIs 
+# ###### read in POIs 
+# pois = pd.read_csv("edinburgh_pois.csv")
+
+# poi_weights = pois['category'].apply(designate_weight)
+# coords = pois[['lat', 'lon']].to_numpy()
+
+# locations_gdf = cluster_and_get_centremost_points(coords, NUM_LOCATIONS, poi_weights)
+
+# hist_starts_gdf = get_historical_trips()
+
+# # Assign the historical trips to the nearest location
+# near_labs = assign_points_to_nearest_location(hist_starts_gdf, locations_gdf)
+
+# # Use the number of trips that started nearby as a proxy for "desire"/"demand"
+# locs_with_demand, counts = np.unique(near_labs, return_counts = True)
+
+# desire_for_locs = np.zeros(NUM_LOCATIONS)
+# desire_for_locs[locs_with_demand] = counts
+
+
+# # get the distance matrix from this precomputed one of 100x100
+# folder = path("good_example_data", "05 Nov 2025 03-50PM")
+# all_dists = np.load(path(folder, "all_dists.npy"))[:NUM_LOCATIONS,:NUM_LOCATIONS]
+
+# # Run the model
+# sol, mip_gap, near = IP_models.create_and_solve_first_model(
+#     desire_for_locs, all_dists,
+#     bike_max=BIKE_MAX, cost_bike=COST_BIKE, cost_station=COST_STATION, 
+#     budget=BUDGET, dist_max=DIST_MAX
+# )
+
+# print(sol[sol["desire"]>0])
+
+# # Colours: historical trips are the colour of the location they are assigned to
+# cmap = plt.colormaps['cool'].resampled( len(locs_with_demand) )
+# cols_for_locs_with_demand  = cmap(np.arange(len(locs_with_demand)))
+# cols_for_locations = np.zeros((NUM_LOCATIONS,4))
+# # locations with no demand/desire are shown in grey
+# cols_for_locations[:,:] = .5
+# cols_for_locations[:,3] = .5
+
+
+# cols_for_locations[locs_with_demand,:] = cols_for_locs_with_demand
+# cols_for_hist_data = cols_for_locations[near_labs]
+
+
+# # Plot the solution
+# built = sol["build"].to_numpy() == 1
+# _, ax1 = plt.subplots()
+
+# hist_starts_gdf.to_crs(locations_gdf.crs).plot(
+#     ax=ax1, label="Historical",
+#     color=cols_for_hist_data,
+#     markersize=10
+#     ,marker="."
+# )
+
+# locations_gdf[~built].plot(
+#     ax=ax1, label="Unused Locations",
+#     color=cols_for_locations[~built],
+#     markersize=30,
+#     marker = "x"
+# )
+
+# locations_gdf[built].plot(
+#     ax=ax1, label="Station Locations",
+#     color=cols_for_locations[built],
+#     markersize=40,
+#     marker = "^"
+# )
+
+# ctx.add_basemap(ax1, source=ctx.providers.CartoDB.Positron)
+# ax1.legend()
+# plt.title("Colours show assignment")
+# plt.show()
+
+
+
+#### Extended model (or whatever we want to call it)
 pois = pd.read_csv("edinburgh_pois.csv")
-
-poi_weights = pois['category'].apply(designate_weight)
-coords = pois[['lat', 'lon']].to_numpy()
-
-locations_gdf = cluster_and_get_centremost_points(coords, NUM_LOCATIONS, poi_weights)
-
-hist_starts_gdf = get_historical_trips()
-
-# Assign the historical trips to the nearest location
-near_labs = assign_points_to_nearest_location(hist_starts_gdf, locations_gdf)
-
-# Use the number of trips that started nearby as a proxy for "desire"/"demand"
-locs_with_demand, counts = np.unique(near_labs, return_counts = True)
-
-desire_for_locs = np.zeros(NUM_LOCATIONS)
-desire_for_locs[locs_with_demand] = counts
+pois = gpd.GeoDataFrame(
+    pois,
+    geometry= [Point(lon, lat) for lat, lon in pois[["lat", "lon"]].to_numpy()]
+    ,crs="EPSG:4326"  # WGS84 Latitude/Longitude
+).to_crs(epsg=3857)
+pois["latitude"] = pois["lat"]
+pois["longitude"] = pois["lon"]
 
 
-# get the distance matrix from this precomputed one of 100x100
-folder = path("good_example_data", "05 Nov 2025 03-50PM")
-all_dists = np.load(path(folder, "all_dists.npy"))[:NUM_LOCATIONS,:NUM_LOCATIONS]
+rng = np.random.default_rng(2025)
+subset = rng.choice(pois.shape[0], size = 1_000)
+pois = pois.iloc[subset].reset_index(drop=True)
+print(f"{pois.shape=}")
 
-# Run the model
-sol, mip_gap, near = IP_models.create_and_solve_model(
-    desire_for_locs, all_dists,
-    bike_max=BIKE_MAX, cost_bike=COST_BIKE, cost_station=COST_STATION, 
-    budget=BUDGET, dist_max=DIST_MAX
+castle = np.array([-3.200833, 55.94861])
+near_castle = haversine_np(pois["longitude"], pois["latitude"], castle[0], castle[1]) <= 2
+dists_locs = get_dists_gps(pois)
+
+demand = predict_bike_count_MLP(pois[["latitude", "longitude"]].to_numpy())
+
+
+sol, mip, near = IP_models.create_and_solve_extended_model(
+    desire = demand,
+    dist_mat= dists_locs,
+    near_centre=near_castle,
+    bikes_total=800, bikes_max = 50,
+    cost_bike=600, cost_station=20_000,
+    budget=2_000_000
 )
 
-print(sol[sol["desire"]>0])
-
-# Colours: historical trips are the colour of the location they are assigned to
-cmap = plt.colormaps['cool'].resampled( len(locs_with_demand) )
-cols_for_locs_with_demand  = cmap(np.arange(len(locs_with_demand)))
-cols_for_locations = np.zeros((NUM_LOCATIONS,4))
-# locations with no demand/desire are shown in grey
-cols_for_locations[:,:] = .5
-cols_for_locations[:,3] = .5
-
-
-cols_for_locations[locs_with_demand,:] = cols_for_locs_with_demand
-cols_for_hist_data = cols_for_locations[near_labs]
 
 
 # Plot the solution
 built = sol["build"].to_numpy() == 1
+print(sol[built])
 _, ax1 = plt.subplots()
 
-hist_starts_gdf.to_crs(locations_gdf.crs).plot(
-    ax=ax1, label="Historical",
-    color=cols_for_hist_data,
-    markersize=10
-    ,marker="."
-)
 
-locations_gdf[~built].plot(
+pois[~built].plot(
     ax=ax1, label="Unused Locations",
-    color=cols_for_locations[~built],
     markersize=30,
     marker = "x"
 )
 
-locations_gdf[built].plot(
+pois[built].plot(
     ax=ax1, label="Station Locations",
-    color=cols_for_locations[built],
     markersize=40,
     marker = "^"
 )
 
 ctx.add_basemap(ax1, source=ctx.providers.CartoDB.Positron)
 ax1.legend()
-plt.title("Colours show assignment")
 plt.show()
