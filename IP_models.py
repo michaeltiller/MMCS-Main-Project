9eb96609e56c26eb559c8159afab9e14c41954d6
Naoise <s2848034@ed.ac.uk>
2025-11-20 21:38:02 +0000

    move extended model into IPmodels

--- IP_models.py
+++ IP_models.py
@@ -5,6 +5,7 @@ from shapely import Point
 from time import perf_counter
 from helper_functions import *
 import platform
+from itertools import combinations
 
 if platform.system() == "Windows":
     xp.init('C:/xpressmp//bin/xpauth.xpr')
@@ -211,6 +212,274 @@ def create_and_solve_basic_distmin_model(desire, dist_mat, near_centre, cost_bik
 
     # return the pertient info that was not inputted
     return solution, MIP_gap
+
+
+
+def create_and_solve_extended_model(desire, dist_mat, bike_max, 
+                                    cost_bike, cost_station, budget,
+                                    rev_per_bike, near_to_trains,
+                                    dist_min =100, dist_max=5_000):
+
+    # stop the big stream of text
+    xp.setOutputEnabled(False)
+    prob = xp.problem("First_bike_extension") 
+
+    num_locs = desire.shape[0]
+    I = set(range(num_locs))
+    Trains = range(near_to_trains.shape[0])
+
+    ########### Decision variables #############
+
+    # we have the binary decision variable to build a station at location i
+    build = np.array(
+        [prob.addVariable(name = f"build_{i}", vartype = xp.binary) for i in I ]
+        ,dtype = xp.npvar
+    )
+    #we have the integer decision variable of how many bikes to place at location i
+    bikes = np.array(
+        [prob.addVariable(name = f"bikes_{i}", vartype= xp.integer) for i in I]
+        , dtype=xp.npvar
+    )
+    
+    
+    allocated = np.array(
+        [[prob.addVariable(name=f'allocated_{i}_{j}', vartype=xp.binary) for j in I]
+        for i in I]
+    )
+    
+    # we may choose to build near / cover a train station
+    train_covered = np.array(
+        [prob.addVariable(name=f"covered_train_{t}", vartype=xp.binary) for t in Trains]
+        , dtype=xp.npvar
+    )
+    gamma = 10_000  # reward for covering a train station
+
+
+
+    ########### Objective function #############
+    # prob.setObjective(
+    #     xp.Sum(desire[i] * bikes[i] for i in I) + gamma * xp.Sum(train_covered[t] for t in Trains),
+    #     sense=xp.maximize
+    # )
+
+
+    ########### Soft train-station coverage #############
+    # we can cover a train station by building near it
+    for t in Trains:
+        nearby_locs = np.where(near_to_trains[t])[0]
+        if nearby_locs.size > 0:
+            prob.addConstraint(train_covered[t] <= xp.Sum(build[i] for i in nearby_locs))
+        else:
+            #if there are no stations near it we can never cover it
+            prob.addConstraint(train_covered[t] == 0)
+
+    
+    
+    # if u want it to run fast use the below objective function 
+    prob.setObjective(
+        xp.Sum( (rev_per_bike * desire[i] - cost_bike) * bikes[i] for i in I ) - xp.Sum(cost_station * build[i] for i in I ) 
+        , sense = xp.maximize
+    )
+    ########### constraints #############
+
+    # we can place at most bikes_max bikes in each location
+    # and if we put bikes somewhere then we must build a station there
+    prob.addConstraint(
+        bikes[i] <= bike_max*build[i] for i in I
+    )
+
+    # If we build a station somewhere then we must put at least one bike there
+    prob.addConstraint(
+        build[i] <= bikes[i] for i in I
+    )
+    
+    # we will not put more bikes in a location than there is desire for bikes
+    # the plus one is to avoid issues with combining the above constraint and the connectedness constraint
+    prob.addConstraint(
+        bikes[i] <= desire[i] + 1 for i in I
+    )
+
+        
+    # stay within budget
+    prob.addConstraint(
+        xp.Sum( cost_bike*bikes[i] + cost_station*build[i] for i in I) <= budget
+    )
+    
+    # Basic min Distance constraints #####
+    
+    too_close = np.zeros_like(dist_mat, dtype=int)
+    for i in I:
+        for j in I:
+            if i < j:
+                if dist_mat[i,j] < dist_min:
+                    too_close[i,j] = 1
+                    too_close[j,i] = 1
+
+    
+    prob.addConstraint(build[i] + xp.Sum(too_close[i,j]*build[j] for j in I if j != i) <= 1 for i in I)
+
+
+    ## connectivity Constraints ####
+    
+    prob.addConstraint(allocated[i,j] <= build[i] for i in I for j in I)
+    prob.addConstraint(allocated[i,j] <= build[j] for i in I for j in I)
+    
+    
+    prob.addConstraint(
+        build[j] == xp.Sum(allocated[j, i] for i in I) 
+        for j in I 
+    )
+    
+    # this is what fucked it - having the the below constraint prevents the spoke style connections 
+    # prob.addConstraint(
+    #     build[j] == xp.Sum(allocated[i,j] for i in I) 
+    #     for j in I 
+    # )
+    prob.addConstraint(
+        allocated[i, j] + allocated[j, i] <= 1 for i in I for j in I
+    )
+    
+    # Distance constraints
+    prob.addConstraint( 
+        build[j] * dist_min <= xp.Sum(allocated[j, i] * dist_mat[i, j] for i in I)
+        for j in I 
+    )
+    prob.addConstraint( 
+        build[j] * dist_max >= xp.Sum(allocated[j, i] * dist_mat[i, j] for i in I)
+        for j in I 
+    )
+    
+  
+    
+    
+ 
+    ########## Solving ###########
+    # Write problem statement to file, for debugging
+    # prob.write("problem","ips")
+ 
+    print("Solving first without connectedness")
+    solve_start = perf_counter()
+    prob.solve()
+    solve_end = perf_counter()
+    print(f"Solved in {solve_end-solve_start:.0f} seconds with {desire.shape[0]:,} variables")
+
+    #mip gap 
+    MIP_gap= get_MIP_gap(prob)
+    print(f"{MIP_gap=:.2%}")
+
+    
+
+    temp = prob.getSolution(allocated)
+
+    ######### Cycling contraint ##########
+    arcs = []
+
+    for i in I:
+        row_i = temp[i]
+        j = np.where(row_i)[0]
+        if j.any():
+            arcs.append((i,j[0].item()))
+    
+    # size of subsets where we do not want a loop
+    size_s = 3
+    
+    
+   
+    print("adding connectedness constraints")
+    s_connect=perf_counter()
+    #I suspect that in the second solution we build in areas we hadn't before 
+    # - areas not subject to the below constraints
+    # hence we see cycles
+
+    originally_built = prob.getSolution(build).nonzero()[0]
+    print(f" {len(originally_built)=:,} ")
+
+    all_built_subsets = combinations(originally_built , size_s)
+    
+    # each village must have an arc inwards from outside the village
+    # count = 0
+    for s in all_built_subsets:
+
+        outside_s = I.difference(set(s))
+        prob.addConstraint(
+            xp.Sum(allocated[i, j] for i in outside_s for j in s)
+            <=
+            xp.Sum( build[i] for i in s) / size_s
+        )    
+    #     count +=1
+
+    # map(
+    #     lambda s : prob.addConstraint(
+    #         xp.Sum(allocated[i, j] for i in I.difference(set(s)) for j in s)
+    #         <=
+    #         xp.Sum( build[i] for i in s) / size_s
+    #     )
+    #     , all_built_subsets
+    # )
+
+    
+    # no loops in a village / subset s
+    prob.addConstraint(
+        xp.Sum(allocated[i,j] for i in s for j in s )
+        <= size_s - 1
+        for s in all_built_subsets
+    )
+    e_connect = perf_counter()
+    print(f"done adding connectedness {e_connect-s_connect:.0f} secs")
+    # for s in all_subsets:
+        
+    #     s = all_subsets[0]
+    #     notinS = [el for el in G.nodes() if el not in s]
+    #     prob.addConstraint(
+    #         xp.Sum(allocated[i, j] for i in notinS for j in s)
+    #         <=
+    #         xp.Sum( build[i] for i in s) / size_s
+    #     )
+        
+    #     # no loops in a village
+    #     prob.addConstraint(
+    #         xp.Sum(allocated[i,j] for i in s for j in s )
+    #         <= size_s - 1
+    #     )
+    
+    
+
+    print("Resolving")
+    solve_start = perf_counter()
+    prob.solve()
+    solve_end = perf_counter()
+    print(f"Solved in {solve_end-solve_start:.0f} seconds with {desire.shape[0]:,} variables")
+
+    #mip gap 
+    MIP_gap= get_MIP_gap(prob)
+    print(f"{MIP_gap=:.2%}")
+
+    # look at the solution
+    solution  = pd.DataFrame({
+        "build": np.array([ int(i) for i in prob.getSolution(build) ]),
+        "bikes": np.array([int(i) for i in prob.getSolution(bikes) ]),
+        "desire": desire
+    })
+    
+    alloc_solution = np.zeros_like(allocated, dtype=bool)
+    temp = prob.getSolution(allocated)
+    
+    
+    for i in I:
+        for j in I:
+            alloc_solution[i, j] = bool(temp[i, j])
+    del temp
+
+    # Put into a DataFrame for readability
+    alloc_df = pd.DataFrame(
+        alloc_solution,
+        columns=[f"to_{j}" for j in I],
+        index=[f"from_{i}" for i in I]
+    )
+   
+    
+    # return the pertient info that was not inputted
+    return solution, MIP_gap, alloc_df, arcs
 
 
 
